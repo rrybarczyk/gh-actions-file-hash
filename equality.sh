4e9de9664e362e5a1b41d83fffe57bcc02685476
RJ Rybarczyk <rj@rybar.tech>
2021-08-25 16:39:52 +0000

    Add back in cbindgen

--- equality.sh
+++ equality.sh
@@ -1,9 +1,9 @@
 set -e
-# cargo install cbindgen --force bts
-# cbindgen -V
+cargo install cbindgen --force bts
+cbindgen -V
+
 cd src
 SHA1_1=$(sha1sum file.h)
-# SHA1_1=$(realpath src/file.h | sha1sum)
 cd ..
 BUILD_SCRIPT="./build-header.sh"
 sh ./"$BUILD_SCRIPT"
